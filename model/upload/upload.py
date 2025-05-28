edfc2f07113cfb0475db7557203a106c3a988133
UICHEOL-HWANG <icuchoel@gmail.com>
2025-05-28 18:45:19 +0900

    Chore: upload - argparser 추가

--- model/upload/upload.py
+++ model/upload/upload.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import argparse
 
 def upload_to_model(upload_path, model_path):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -8,6 +9,12 @@ def upload_to_model(upload_path, model_path):
     tokenizer.push_to_hub(upload_path)
 
 
-if __name__ == "__main__":
-    upload_to_model("UICHEOL-HWANG/EcomGen-0.0.1v", "../Polyglot-ko-1.3b/merged_polyglot")
-
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='모델이 너무 많아서 이렇게 올릴랭')
+    
+    parser.add_argument('--upload_path', type=str, required=True,
+                        help='허브에 업로드할 경로')
+    parser.add_argument('--model_path', type=str, required=True,
+                        help='업로드할 로컬 모델의 경로')
+    
+    return parser.parse_args()
